cd1074bc243ec5804f173349d5ffffc84f219ea4
Will B <wberman27@gmail.com>
2021-03-12 10:12:16 -0500

    added header test for CheckoutForm

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+describe("Sprint Challenge Tests", () =>{
+    
+    test("form header renders", () => {
+        render(<CheckoutForm />)
+        const header = screen.queryByText(/checkout form/i)
+        expect(header).toBeInTheDocument();
+    
+    
+    });
+        
+    test("form shows success message on submit with form details", () => {
+        render(<CheckoutForm />)
+    
+    });
 
-test("form shows success message on submit with form details", () => {});
+})
